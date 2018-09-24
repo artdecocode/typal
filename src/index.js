c152c11bc5927610128a3daeb36740289307a740
Anton <anton@adc.sh>
2018-09-24 23:19:43 +0300

    add implementation

--- src/index.js
+++ src/index.js
@@ -1,22 +1,2 @@
-import { debuglog } from 'util'
-
-const LOG = debuglog('typal')
-
-/**
- * Keeps JSDoc types in XML files and converts them to JavaScript and Markdown.
- * @param {Config} config Options for the program.
- * @param {boolean} config.shouldRun A boolean option.
- */
-export default async function typal(config) {
-  const {
-    type,
-  } = config
-  LOG('typal called with %s', type)
-  return type
-}
-
-/* documentary types/index.xml */
-/**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} shouldRun A boolean option.
- */
+export { default as Type } from './lib/Type'
+export { default as Property } from './lib/Property'
