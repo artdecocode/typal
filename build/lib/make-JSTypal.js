640b76e41153c0b4ba658cb694fa51663a274227
Anton <anton@adc.sh>
2019-04-30 12:37:13 +0300

    b

--- build/lib/make-JSTypal.js
+++ build/lib/make-JSTypal.js
@@ -2,7 +2,8 @@ const typedefJsRule = require('./typedef/rule');
 const JSDocRule = require('./typedef/jsdoc');
 const JSTypal = require('./JSTypal');
 
-module.exports=(conf) => {
-  const jsTypal = new JSTypal([typedefJsRule, JSDocRule], conf)
+module.exports=(conf, onlyTypedef = false) => {
+  const rules = onlyTypedef ? [typedefJsRule] : [typedefJsRule, JSDocRule]
+  const jsTypal = new JSTypal(rules, conf)
   return jsTypal
 }
