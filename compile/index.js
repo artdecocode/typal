9826948b2cee84a614bad524385efe61fb2514e5
Anton <anton@adc.sh>
2019-08-10 05:59:59 +0300

    getLinks api

--- compile/index.js
+++ compile/index.js
@@ -1,4 +1,4 @@
-const { _parseFile } = require('./depack')
+const { _parseFile, _getLinks } = require('./depack')
 
 /**
  * Parses the types.xml file. Looks for `<type>`, `<constructor>`, `<interface>` and `<method>` elements and extracts their properties, functions and arguments.
@@ -14,13 +14,25 @@ function parseFile(xml, namespace) {
   return _parseFile(xml, namespace)
 }
 
+/**
+ * Gets links for markdown. Iterates through the types to find referenced ones, and returns a string which contains a link to it.
+ * @param {!Array<_typal.Type>} allTypes All types that can be linked.
+ * @param {string|!_typedefsParser.Type} type The type or parsed type that should be serialised.
+ * @param {!_typal.LinkingOptions=} [opts] Options for linking.
+ * @return {string}
+ */
+function getLinks(allTypes, type, opts = {}) {
+  return _getLinks(allTypes, type, opts)
+}
+
 module.exports = {
   parseFile,
+  getLinks,
 }
 
 /**
  * @typedef {import('../types').Type} _typal.Type
  */
 /**
- * @typedef {import('../types').Import} _typal.Import
+ * @typedef {import('../types').LinkingOptions} _typal.LinkingOptions
  */
--- compile/template.js
+++ compile/template.js
@@ -10,8 +10,8 @@ function parseFile(xml, namespace) {
 /**
  * @methodType {getLinks}
  */
-function getLinks(xml, namespace) {
-  return _getLinks(xml, namespace)
+function getLinks(allTypes, type, opts = {}) {
+  return _getLinks(allTypes, type, opts)
 }
 
 module.exports = {
@@ -21,4 +21,7 @@ module.exports = {
 
 /**
  * @typedef {import('../types').Type} _typal.Type
+ */
+/**
+ * @typedef {import('../types').LinkingOptions} _typal.LinkingOptions
  */
